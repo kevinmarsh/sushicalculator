a54f35b5408981bdeccc108457f08433be3ed97e
Kevin Marsh <kevinmarsh3@gmail.com>
2013-07-31 12:35:15 +0100

    added in firebase as the backend for data

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var sushiApp = angular.module('sushiApp', []);
+var sushiApp = angular.module('sushiApp', ['firebase']);
 
 sushiApp.config( function($routeProvider){
     // TODO: add html5 links --> $locationProvider.html5Mode(true)
@@ -17,38 +17,41 @@ sushiApp.config( function($routeProvider){
         .otherwise({redirectTo: '/'});
 });
 
-sushiApp.controller('SushiPlateCtrl', function($scope) {
-    // TODO: store plate counts in local storage
-    $scope.plates = [
-        {name: 'Green', price: 1.90, count: 0},
-        {name: 'Blue', price: 2.50, count: 0},
-        {name: 'Purple', price: 3.10, count: 0},
-        {name: 'Orange', price: 3.60, count: 0},
-        {name: 'Pink', price: 4.10, count: 0},
-        {name: 'Grey', price: 5.00, count: 0}
-    ];
-    $scope.getTotalPlates = function() {
-        var totalPlates = 0;
-        for (var i = 0, length = $scope.plates.length; i < length; i++) {
-            totalPlates += $scope.plates[i].count;
+sushiApp.controller('SushiPlateCtrl', function($scope, angularFire) {
+    var url = 'https://bbd.firebaseio.com/sushiCalulator/plates';
+    var promise = angularFire(url, $scope, 'plates', []);
+    promise.then(function() {
+        $scope.getTotalPlates = function() {
+            var totalPlates = 0;
+            for (var i = 0, length = $scope.plates.length; i < length; i++) {
+                totalPlates += $scope.plates[i].count;
+            }
+            return totalPlates;
+        };
+        $scope.getTotalPrice = function() {
+            var totalPrice = 0;
+            for (var i = 0, length = $scope.plates.length; i < length; i++) {
+                totalPrice += $scope.plates[i].price * $scope.plates[i].count;
+            }
+            return totalPrice;
+        };
+        $scope.changeCount = function(plate, x) {
+            if (plate.count + x >= 0) {
+                plate.count += x;
+            }
+        };
+        $scope.clearPlates = function() {
+            for (var i = 0, length = $scope.plates.length; i < length; i++) {
+                $scope.plates[i].count = 0;
+            }
         }
-        return totalPlates;
-    };
-    $scope.getTotalPrice = function() {
-        var totalPrice = 0;
-        for (var i = 0, length = $scope.plates.length; i < length; i++) {
-            totalPrice += $scope.plates[i].price * $scope.plates[i].count;
-        }
-        return totalPrice;
-    };
-    $scope.changeCount = function(plate, x) {
-        if (plate.count + x >= 0) {
-            plate.count += x;
-        }
-    };
-    $scope.clearPlates = function() {
-        for (var i = 0, length = $scope.plates.length; i < length; i++) {
-            $scope.plates[i].count = 0;
-        }
-    }
+    });
+});
+
+sushiApp.controller('HighScoreCtrl', function($scope, angularFire) {
+    var url = 'https://bbd.firebaseio.com/sushiCalulator/highscore';
+    var promise = angularFire(url, $scope, 'highscores', []);
+    promise.then(function() {
+        // TODO: save to highscore, reset high scores
+    });
 });
